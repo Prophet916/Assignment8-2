85224682c179155fb839ee1e24bbad83b3cfd816
tach9 <tach9@DESKTOP-1AEJVRA>
2023-07-30 19:14:07 -0600

    Add project files.

---
+++ MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_2
+{
+    internal class MergeSort
+    {
+        public static void MergeSorted(int[] arr)
+        {
+            if (arr == null || arr.Length <= 1)
+                return;
+
+            int n = arr.Length;
+            int[] temp = new int[n];
+            MergeSortHelper(arr, temp, 0, n - 1);
+        }
+
+        private static void MergeSortHelper(int[] arr, int[] temp, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = left + (right - left) / 2; // Avoids potential overflow
+
+                MergeSortHelper(arr, temp, left, mid);
+                MergeSortHelper(arr, temp, mid + 1, right);
+
+                Merge(arr, temp, left, mid, right);
+            }
+        }
+
+        private static void Merge(int[] arr, int[] temp, int left, int mid, int right)
+        {
+            Array.Copy(arr, left, temp, left, right - left + 1);
+
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+
+            while (i <= mid && j <= right)
+            {
+                if (temp[i] <= temp[j])
+                {
+                    arr[k] = temp[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = temp[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i <= mid)
+            {
+                arr[k] = temp[i];
+                i++;
+                k++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] arr = { 12, 11, 13, 5, 6, 7 };
+                Console.WriteLine("Original Array:");
+                PrintArray(arr);
+
+                MergeSort.MergeSorted(arr);
+                Console.WriteLine("\nSorted Array:");
+                PrintArray(arr);
+                Console.ReadKey();
+            }
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
